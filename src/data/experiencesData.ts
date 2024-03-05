0208ccbc6b0c634283838edf2ba8462e7bb1863f
MamedYahyayev <mammadyahyayev@gmail.com>
2024-03-05 22:02:38 +0400

    Add begin and end date into experience data, close #2

--- src/data/experiencesData.ts
+++ src/data/experiencesData.ts
@@ -5,6 +5,8 @@ export interface Experience {
     link: string,
     role: string;
     duration: string;
+    beginDate: Date;
+    endDate?: Date;
     description?: string;
     tags: string[]
 }
@@ -20,6 +22,7 @@ export const employmentHistoryData: EmploymentHistory[] = [
         company: "AISTGroup",
         link: "https://aist.group/",
         duration: "1 months",
+        beginDate: new Date(2024, 2),
         description: "",
         tags: ['Java', 'Spring Framework', 'RabbitMQ']
     },
@@ -30,11 +33,13 @@ export const employmentHistoryData: EmploymentHistory[] = [
         company: "SmartScoring",
         link: "https://smartscoring.com/",
         duration: "2 years",
+        beginDate: new Date(2022, 3),
+        endDate: new Date(2024, 2),
         description: "Develop and maintain desktop based applications.\n" +
-        "Decreased deployment time by 95% through implementation of easy-to-understand scripting\n" +
-        "Upgrade and maintain existing systems to ensure that they meet evolving business needs.\n" +
-        "Write and implement efficient code to ensure that code is scalable, maintainable, and secure.\n" +
-        "Continuously improve software development processes by identifying areas for improvement.",
+            "Decreased deployment time by 95% through implementation of easy-to-understand scripting\n" +
+            "Upgrade and maintain existing systems to ensure that they meet evolving business needs.\n" +
+            "Write and implement efficient code to ensure that code is scalable, maintainable, and secure.\n" +
+            "Continuously improve software development processes by identifying areas for improvement.",
         tags: ['C#', '.NET', 'WPF', 'Java', 'gRPC', 'TypeScript', 'Angular']
     },
     {
@@ -44,12 +49,45 @@ export const employmentHistoryData: EmploymentHistory[] = [
         company: "Logix",
         link: "http://logix.az/",
         duration: "4 months",
+        beginDate: new Date(2022, 0),
+        endDate: new Date(2022, 3),
         description: "Design, develop and maintain SQL scripts, stored procedures, and functions.\n" +
             "Monitor and optimize database performance, identifying and addressing potential bottlenecks or issues.\n" +
             "Manage database security, ensuring that data is protected and only accessible to authorized users.",
         tags: ['RDBMS', 'Sql Server']
     }
 ]
+
+export const showDurationInStr = (beginDate: Date, endDate: Date): string => {
+    if (!beginDate || !endDate) {
+        throw new Error("beginDate or endDate can't be null!")
+    }
+
+    const yearOfBeginDate = beginDate.getFullYear();
+    const monthOfBeginDate = beginDate.getMonth();
+
+
+    const yearOfEndDate = endDate.getFullYear();
+    const monthOfEndDate = endDate.getMonth();
+
+
+    const diffInYears = yearOfEndDate - yearOfBeginDate;
+    const diffInMonths = Math.abs(monthOfEndDate - monthOfBeginDate);
+
+    let result = '';
+
+    if (diffInYears > 0) {
+        result += diffInYears == 1 ? "1 year" : diffInYears + " years";
+    }
+
+    if (diffInMonths > 0) {
+        result += diffInMonths == 1 ? "1 month" : diffInMonths + " months";
+    }
+
+
+    return result;
+}
+
 
 //
 // export const educationHistoryData: EducationHistory[] = [
