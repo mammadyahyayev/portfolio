13c83a8ef2f0e37f4cc554b150aa2958630ea664
MamedYahyayev <yahyayev2001@gmail.com>
2024-04-25 22:43:26 +0400

    Convert react to next js

---
+++ src/app/layout.tsx
@@ -0,0 +1,21 @@
+import '../index.css'
+import { Metadata } from "next"
+
+export const metadata: Metadata = {
+    title: "Mammad Yahyayev's blog",
+    description: 'Plenty of blogs waits you like treasure!',
+}
+
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+    return (
+        <html lang="en">
+            <head>
+                <title>Mammad Yahyayev</title>
+            </head>
+            <body>
+                <div id="root">{children}</div>
+                <script type="module" src="/src/main.tsx"></script>
+            </body>
+        </html>
+    )
+}
