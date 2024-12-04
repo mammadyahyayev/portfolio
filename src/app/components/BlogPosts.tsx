2b3fda589b71028876373f49baaaa00a7d801a2c
mammadyahyayev <mammadyahyayev@gmail.com>
2024-12-04 14:18:54 +0400

    feat: wip: fetch medium posts

--- src/app/components/BlogPosts.tsx
+++ src/app/components/BlogPosts.tsx
@@ -1,7 +1,22 @@
+import React, { useEffect } from "react";
 import BlogPost from "./BlogPost.tsx";
-import {blogPostsData} from "../data/blogsData.ts";
+import { blogPostsData } from "../data/blogsData.ts";
+import { MediumClient, PublishedPost } from "medium-sdk-ts";
 
 const BlogPosts = () => {
+
+    const [posts, setPosts] = React.useState<PublishedPost[]>([])
+
+    useEffect(() => {
+        const client = new MediumClient();
+        client.getPosts("mammadyahya")
+            .then(mediumPosts => {
+                console.log(`Fetched ${mediumPosts.length} posts`)
+                setPosts(mediumPosts)
+            })
+            .catch(err => console.error("Failed to fetch medium posts", err))
+    }, [])
+
     return (
         <ul className="group/list">
             {blogPostsData.sort((a, b) => b.date.getTime() - a.date.getTime()).slice(0, 3).map(post => (
